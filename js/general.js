74af0a0f137af7a1b5476b91965f8fa40486de2a
Yung Yi <yungyi05@gmail.com>
2018-09-10 22:25:42 -0500

    Add the documentation to general.js file

--- js/general.js
+++ js/general.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+	// Get the position value from the top
     var sticky = $("nav").offset().top;
 
   	$(window).scroll(function(){
+  		// Compare the pixel value that the current document has 
+  		// been scrolled to the sticky position, then add sticky 
+  		// class according to the result.
         if ($(window).scrollTop() >= sticky) {
           	$("nav").addClass("sticky")
         } else {
